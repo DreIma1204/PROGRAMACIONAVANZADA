15525bf3d74e3ed32322c3e4fa090f2a5788c1e4
Andre Imanol Rodriguez Lopez <ar391534@gmail.com>
2026-02-27 18:35:16 -0600

    a1_28

---
+++ Programacionavanzada/Actividades/Actividad1Clase27/src/Main.java
@@ -0,0 +1,18 @@
+import Parte2Vista.Practica03_d;
+import javax.swing.UIManager;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Practica03_d();
+            }
+        });
+    }
+}
